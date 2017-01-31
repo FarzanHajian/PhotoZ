d38a4bd64f9fbec24d7a1afb940385a914ca7fde
FarzanHajian <fh791@yahoo.com>
2017-01-31 14:03:49 +0330

    Adding delete functionality

---
+++ PhotoZ/Models/FileViewInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace PhotoZ.Models
+{
+    public class FileViewInfo
+    {
+        public string Id { get; set; }
+
+        public string Filename { get; set; }
+    }
+}
---
+++ PhotoZ/Scripts/site.js
@@ -0,0 +1,20 @@
+﻿function initializeModal() {
+    var modal = document.getElementById('myModal');
+    var modalImg = document.getElementById("imgModal");
+    var captionText = document.getElementById("caption");
+
+    var thumbnails = document.querySelectorAll('[thumbnail]');
+    for (index in thumbnails) {
+        thumbnails[index].onclick = function () {
+            modal.style.display = "block";
+            modalImg.src = this.src;
+            captionText.innerHTML = this.alt;
+        };
+    }
+
+    var span = document.getElementsByClassName("close")[0];
+
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+}
